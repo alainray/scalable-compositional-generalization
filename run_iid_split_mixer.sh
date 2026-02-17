8a1712d20b653a1488e1eac0fa5c572258e36825
Alain <alainray@gmail.com>
2026-02-17 15:41:21 -0300

    multiple bash scripts added

---
+++ run_iid_split_mixer.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=run_comp
+#SBATCH --output=exp_logs/%x_%j.out
+#SBATCH --error=exp_logs/%x_%j.err
+#SBATCH --time=2-00:00:00
+#SBATCH --account=forgetting_pixels_learnin_vidt5
+#SBATCH --partition=debug
+#SBATCH --nodelist=peteroa
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:1
+#SBATCH --mem=30G
+# Activar entorno si es necesario
+source ~/storage/torch/bin/activate
+cd ~/investigacion/scalable-compositional-generalization
+
+WANDB_DEBUG=1,WANDB_SILENT=false ./iid_split_mixer_runner.sh $ds
+wait
+
+echo "Finished with job $SLURM_JOBID (seed=$SEED)"
