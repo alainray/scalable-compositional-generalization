b8b25d5a4a9269914d1238e38e87f3c1520701af
Alain Raymond-Sáez <alainray@gmail.com>
2026-02-25 13:25:36 -0300

    Add mixer flag to toggle single-case vs all-case supervision

--- visgen/models/resnet_mixer.py
+++ visgen/models/resnet_mixer.py
@@ -45,6 +45,7 @@ class ResNet18Mixer(BaseModel):
         mixer_loss_weight: float = 1.0,
         mixer_detach_target: bool = False,
         use_mixer_classifier: bool = False,
+        use_all_mixer_cases: bool = False,
         *args,
         **kwargs,
     ) -> None:
@@ -57,6 +58,7 @@ class ResNet18Mixer(BaseModel):
         self.mixer_loss_weight = mixer_loss_weight
         self.mixer_detach_target = mixer_detach_target
         self.use_mixer_classifier = use_mixer_classifier
+        self.use_all_mixer_cases = use_all_mixer_cases
         self.mixer_loss_fn = nn.MSELoss()
         self._logged_metrics = self._logged_metrics + ["mixer_loss", "total_loss"]
 
@@ -105,16 +107,29 @@ class ResNet18Mixer(BaseModel):
             cls_loss, log_dict = self._compute_classification(reps_flat, y)
             mixer_loss = torch.tensor(0.0, device=reps.device)
             if self.mixer is not None and num_views >= 4:
-                mixer_inputs = reps[:, :3, :]
-                target_rep = reps[:, 3, :]
-                if self.mixer_detach_target:
-                    target_rep = target_rep.detach()
-                mixed_rep = self.mixer(mixer_inputs)
-                mixer_loss = self.mixer_loss_fn(mixed_rep, target_rep)
-                if self.use_mixer_classifier and y is not None and y.dim() > 2:
-                    mixer_logits = self._classifier_outputs(mixed_rep)
-                    mixer_cls_loss, _ = self.loss_fn(mixer_logits, y[:, 3, :])
-                    mixer_loss = mixer_loss + mixer_cls_loss
+                if self.use_all_mixer_cases:
+                    case_specs = [
+                        (3, [0, 1, 2]),
+                        (0, [2, 1, 0]),
+                        (1, [2, 3, 0]),
+                        (2, [1, 0, 3]),
+                    ]
+                else:
+                    case_specs = [(3, [0, 1, 2])]
+                mixer_terms = []
+                for target_idx, input_indices in case_specs:
+                    mixer_inputs = reps[:, input_indices, :]
+                    target_rep = reps[:, target_idx, :]
+                    if self.mixer_detach_target:
+                        target_rep = target_rep.detach()
+                    mixed_rep = self.mixer(mixer_inputs)
+                    term_loss = self.mixer_loss_fn(mixed_rep, target_rep)
+                    if self.use_mixer_classifier and y is not None and y.dim() > 2:
+                        mixer_logits = self._classifier_outputs(mixed_rep)
+                        mixer_cls_loss, _ = self.loss_fn(mixer_logits, y[:, target_idx, :])
+                        term_loss = term_loss + mixer_cls_loss
+                    mixer_terms.append(term_loss)
+                mixer_loss = torch.stack(mixer_terms).mean()
             return cls_loss, mixer_loss, log_dict
         reps_flat = self._encode(x)
         cls_loss, log_dict = self._compute_classification(reps_flat, y)
