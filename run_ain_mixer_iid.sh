613450e3ba4988ddebfe00a4183047ca8d01fcce
Alain Raymond <alainray@gmail.com>
2026-04-10 20:16:58 -0400

    more scripts for ablations

---
+++ run_ain_mixer_iid.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=run_comp
+#SBATCH --output=exp_logs/%x_%j.out
+#SBATCH --error=exp_logs/%x_%j.err
+#SBATCH --time=1-00:00:00
+#SBATCH --account=defaultacc
+#SBATCH --partition=debug
+#SBATCH --nodelist=peteroa
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:1
+#SBATCH --mem=50G
+# Activar entorno si es necesario
+source ~/storage/torch/bin/activate
+cd ~/investigacion/scalable-compositional-generalization
+
+WANDB_DEBUG=1,WANDB_SILENT=false ./ain_mixer_iid_runner.sh $ds comp_ablation $dims
+wait
+
+echo "Finished with job $SLURM_JOBID (seed=$SEED)"
