22ead4707518fa411df27b0fd7dc9fa8b188ff4f
Alain <alainray@gmail.com>
2026-04-23 21:32:18 -0400

    added scripts for ablations in AIN

---
+++ run_split_layers.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=run_comp
+#SBATCH --output=exp_logs/%x_%j.out
+#SBATCH --error=exp_logs/%x_%j.err
+#SBATCH --time=02-00:00:00
+#SBATCH --account=forgetting_pixels_learnin_vidt5
+#SBATCH --partition=debug
+#SBATCH --nodelist=peteroa
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:1
+#SBATCH --mem=30G
+# Activar entorno si es necesario
+source ~/storage/torch/bin/activate
+
+cd ~/investigacion/scalable-compositional-generalization
+
+./split_layers.sh $ds comp_ablation $split
+
+wait
+
+echo "Finished with job $SLURM_JOBID (seed=$SEED)"
