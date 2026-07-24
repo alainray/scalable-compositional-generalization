a654164331cd54046005ff55e0c0f0fa91c30ea2
Alain Raymond <alainray@gmail.com>
2026-07-24 10:54:18 -0400

    new experiment scripts for lambda

---
+++ run_ain_alg_lambda.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=lambda
+#SBATCH --output=exp_logs/%x_%j.out
+#SBATCH --error=exp_logs/%x_%j.err
+#SBATCH --time=1-00:00:00
+#SBATCH --account=araymond
+#SBATCH --partition=ialab
+#SBATCH --nodelist=ventress
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:1
+#SBATCH --mem=20G
+# Activar entorno si es necesario
+source ~/storage/torch/bin/activate
+cd ~/investigacion/scalable-compositional-generalization
+
+WANDB_DEBUG=1,WANDB_SILENT=false ./ain_alg_lambda_runner.sh $ds metrics $lambda
+wait
+
+echo "Finished with job $SLURM_JOBID (seed=$SEED)"
